4683fec90cf008a87fc05457e9fb68ebff51c055
qBlackbirdp <blackbird9711@gmail.com>
2024-06-26 15:33:44 +0900

    재귀함수 호출 과정을 보기위한 debug코드 추가

--- src/main/java/com/koreait/Calc.java
+++ src/main/java/com/koreait/Calc.java
@@ -4,9 +4,20 @@ import java.util.Arrays;
 import java.util.List;
 
 public class Calc {
+
+    public static boolean debug = false;
+    public static int runCallCount = 0;
+
     public static int run(String exp) {
+        runCallCount++;
+
         exp = exp.trim();
         exp = stripOuterBrackets(exp);
+
+        if (debug) {
+            System.out.printf("exp(%d) : %s\n", runCallCount, exp);
+        }
+
         // 단일항이 들어오면 바로 리턴
         if (!exp.contains(" ")) {
             return Integer.parseInt(exp);
@@ -17,24 +28,24 @@ public class Calc {
         boolean needToSplit = exp.contains("(") || exp.contains(")");
         boolean needToCompound = needToSum && needToMulti;
 
-//        if (exp.contains("(")) { //괄호가 있다면 실행.
-//            int openBracket = exp.indexOf("("); // 괄호 위치 인덱스 번호로 찾기.
-//            int closeBracket = exp.indexOf(")");
-//            int count = 0; //괄호 개수 세기
-//            for (int i = openBracket; i < exp.length(); i++) { //인덱스 길이만큼 반복하면서 괄호 찾기
-//                if (exp.charAt(i) == '(') count++; //괄호 '(' 추적후 세기.
-//                else if (exp.charAt(i) == ')') count--; // ')'가 시작되면 카운트 감소
-//                if (count == 0) { // '(' 와 ')'의 개수가 같아야 식이 성립하기 때문에 0이될 때를 조건으로 삼음.
-//                    closeBracket = i; // 반복문 반복 횟수가 괄호 마지막 인덱스이기 때문에 그 값을 closeBraket에 넣어줌.
-//                    break; //브레이크
-//                }
-//            }
-//            String expression = exp.substring(openBracket + 1, closeBracket); //첫괄호와 끝괄호 위치를 걷어냄.
-//            int expResult = run(expression); // run메서드 전달 실행.
-//            exp = exp.substring(0, openBracket) + expResult + exp.substring(closeBracket + 1);
-//            //여는 괄호 이전의 수식 + 괄호 수식 결과값 + 닫는 괄호 이후의 수식
-//            return run(exp); // run메서드 전달 실행.
-//        }
+        if (exp.contains("(")) { //괄호가 있다면 실행.
+            int openBracket = exp.indexOf("("); // 괄호 위치 인덱스 번호로 찾기.
+            int closeBracket = exp.indexOf(")");
+            int count = 0; //괄호 개수 세기
+            for (int i = openBracket; i < exp.length(); i++) { //인덱스 길이만큼 반복하면서 괄호 찾기
+                if (exp.charAt(i) == '(') count++; //괄호 '(' 추적후 세기.
+                else if (exp.charAt(i) == ')') count--; // ')'가 시작되면 카운트 감소
+                if (count == 0) { // '(' 와 ')'의 개수가 같아야 식이 성립하기 때문에 0이될 때를 조건으로 삼음.
+                    closeBracket = i; // 반복문 반복 횟수가 괄호 마지막 인덱스이기 때문에 그 값을 closeBraket에 넣어줌.
+                    break; //브레이크
+                }
+            }
+            String expression = exp.substring(openBracket + 1, closeBracket); //첫괄호와 끝괄호 위치를 걷어냄.
+            int expResult = run(expression); // run메서드 전달 실행.
+            exp = exp.substring(0, openBracket) + expResult + exp.substring(closeBracket + 1);
+            //여는 괄호 이전의 수식 + 괄호 수식 결과값 + 닫는 괄호 이후의 수식
+            return run(exp); // run메서드 전달 실행.
+        }
 
         // 강사님 코드
         if (needToSplit) {
@@ -59,7 +70,9 @@ public class Calc {
         if (needToCompound) {
             String[] bits = exp.split(" \\+ ");
 
-
+            char sub = '-';
+            for (int i = 0; i < bits.length; i++) {
+            }
             char multi = '*';
 
 
@@ -126,6 +139,7 @@ public class Calc {
         }
         return -1;
     }
+
     private static String stripOuterBrackets(String exp) {
 //        while (exp.startsWith("(") && exp.endsWith(")")) {
 //            int openCount = 0;
